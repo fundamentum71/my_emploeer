aa5060c7b16ec0876c818ea217d502a095db984d
Aleksandr <it.kucherov@gmail.com>
2022-06-21 11:04:34 +0300

    verstka

---
+++ src/components/emploees-list/emploees-list.js
@@ -0,0 +1,15 @@
+import EmploeesListItem from '../emploees-list-item/emploees-list-item';
+
+import './emploees-list.css';
+
+const EmploeesList = () => {
+	return (
+		<ul className="app-list list-group">
+			<EmploeesListItem />
+			<EmploeesListItem />
+			<EmploeesListItem />
+		</ul>
+	);
+};
+
+export default EmploeesList;
