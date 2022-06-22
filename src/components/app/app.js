0f0be7c87cb09ba618bb76d332507ecc4328d8ff
Aleksandr <it.kucherov@gmail.com>
2022-06-22 09:36:09 +0300

    practice with props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,12 @@ import EmploeesAddForm from '../emploees-add-form/emploees-add-form';
 import './app.css';
 
 function App() {
+	const data = [
+		{ name: 'John C.', salary: 800, increase: false },
+		{ name: 'Alex M.', salary: 3000, increase: false },
+		{ name: 'Carl W.', salary: 5000, increase: false },
+	];
+
 	return (
 		<div className="app">
 			<AppInfo />
@@ -14,7 +20,7 @@ function App() {
 				<SearchPanel />
 				<AppFilter />
 			</div>
-			<EmploeesList />
+			<EmploeesList data={data} />
 			<EmploeesAddForm />
 		</div>
 	);
--- src/components/emploees-list/emploees-list.js
+++ src/components/emploees-list/emploees-list.js
@@ -2,14 +2,11 @@ import EmploeesListItem from '../emploees-list-item/emploees-list-item';
 
 import './emploees-list.css';
 
-const EmploeesList = () => {
-	return (
-		<ul className="app-list list-group">
-			<EmploeesListItem />
-			<EmploeesListItem />
-			<EmploeesListItem />
-		</ul>
-	);
+const EmploeesList = ({ data }) => {
+	const elements = data.map((item) => {
+		return <EmploeesListItem {...item} />;
+	});
+	return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default EmploeesList;
