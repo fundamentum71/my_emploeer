b16a56cf4095de71da652babd4646269cc82dc35
Aleksandr <it.kucherov@gmail.com>
2022-06-23 16:24:54 +0300

    strong metod onToggleIncrease()

--- src/components/emploees-list/emploees-list.js
+++ src/components/emploees-list/emploees-list.js
@@ -2,11 +2,17 @@ import EmploeesListItem from '../emploees-list-item/emploees-list-item';
 
 import './emploees-list.css';
 
-const EmploeesList = ({ data, onDelete }) => {
+const EmploeesList = ({ data, onDelete, onToggleIncrease, onToggleRise }) => {
 	const elements = data.map((item) => {
 		const { id, ...itemPtops } = item;
 		return (
-			<EmploeesListItem key={id} {...itemPtops} onDelete={() => onDelete(id)} />
+			<EmploeesListItem
+				key={id}
+				{...itemPtops}
+				onDelete={() => onDelete(id)}
+				onToggleIncrease={() => onToggleIncrease(id)}
+				onToggleRise={() => onToggleRise(id)}
+			/>
 		);
 	});
 	return <ul className="app-list list-group">{elements}</ul>;
