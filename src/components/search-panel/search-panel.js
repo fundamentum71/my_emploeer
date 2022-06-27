409ab8c46360c804898eecb2ac7653f7ece367dc
Aleksandr <it.kucherov@gmail.com>
2022-06-27 10:19:01 +0300

    add search

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,32 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
-	return (
-		<input
-			type="text"
-			className="form-control seacrh-input"
-			placeholder="Найти сотрудника"
-		/>
-	);
-};
+class SearchPanel extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: '',
+		};
+	}
+
+	onUpdateSearchSP = (e) => {
+		const term = e.target.value;
+		//установка локального состояния
+		this.setState({ term });
+		//пробрасываем локальное состояние наверх
+		this.props.onUpdateSearch(term);
+	};
+	render() {
+		return (
+			<input
+				type="text"
+				className="form-control seacrh-input"
+				placeholder="Найти сотрудника"
+				value={this.state.term}
+				onChange={this.onUpdateSearchSP}
+			/>
+		);
+	}
+}
 
 export default SearchPanel;
